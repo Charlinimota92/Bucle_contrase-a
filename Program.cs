2e410c1264787864934b391d41b4f3ede34be92e
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 18:47:43 -1200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Contraseña");
+Console.WriteLine();
+
+{
+   
+    string contraseñaCorrecta = "No me la sé";
+
+   
+    int intentos = 3;
+
+    while (intentos > 0)
+    {
+        
+        Console.WriteLine("Ingrese la contraseña:");
+        string contraseñaIngresada = Console.ReadLine();
+
+        if (contraseñaIngresada == contraseñaCorrecta)
+        {
+            Console.WriteLine("Contraseña correcta. ¡Acceso concedido!");
+            break; 
+        }
+        else
+        {
+            intentos--;
+            if (intentos > 0)
+            {
+                Console.WriteLine("Contraseña incorrecta. Le quedan " + intentos + " intentos.");
+            }
+            else
+            {
+                Console.WriteLine("Ha agotado el número de intentos. Acceso denegado.");
+            }
+        }
+    }
+}
+
+
+
